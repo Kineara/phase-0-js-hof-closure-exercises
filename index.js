0fe85f6551da1039387119e24cdfc6c2baa57c4b
Lauren Hoock <laurenh8916@protonmail.com>
2022-04-07 16:54:55 -0600

    Initial commit

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-function forEach() {
-
+function forEach(array, func) {
+  func(array);
 }
 
-function map() {
-
+function map(array, func) {
+  let newArray = [];
+  for (const i of array){
+    newArray.push(func(i));
+  }
+  return newArray;
 }
 
-function filter() {
-
+function filter(array, func) {
+  let newArray = [];
+  for (const i of array){
+    if (func(i)){
+      newArray.push(i);
+    }
+  }
+  return newArray;
 }
 
-function every() {
-
+function every(array, func) {
+  for (const i of array) {
+    if(!func(i)) {
+      return false;
+    } 
+  }
+  return true;
 }
 
-function some() {
-
+function some(array, func) {
+  for (const i of array){
+    if (func(i)) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function majority() {
+function majority(array, func) {
+  let truthy = 0;
+  let falsy = 0;
 
+  for (const i of array){
+    func(i) ? truthy++ : falsy++;
+  }
+
+  return truthy > falsy ? true : false;
 }
 
-function once() {
 
+
+function once(func) {
+  
 }
 
 function groupBy() {
